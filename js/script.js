4cceb5b3fd36498868eca196c934dfbbcb68d66f
bernardoklt20-droid <bernardo.klt20@gmail.com>
2026-06-15 16:14:03 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 function agendar() {
     alert("Agendamento realizado com sucesso!");
 }
+
+function atualizarRelogio() {
+
+    let agora = new Date();
+
+    document.getElementById("relogio").innerHTML =
+        "Horário atual: " + agora.toLocaleTimeString();
+}
+
+setInterval(atualizarRelogio, 1000);
